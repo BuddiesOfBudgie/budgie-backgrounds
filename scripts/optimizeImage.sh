ca9bedee763df31f00fb8d45101b37efe896af05
Campbell Jones <git@serebit.com>
2022-09-12 15:45:05 -0400

    Resize images to MINIMUM 3840 width 2160 height

--- scripts/optimizeImage.sh
+++ scripts/optimizeImage.sh
@@ -6,7 +6,7 @@ OUTPUT="$2"
 cp "$INPUT" "$OUTPUT" || exit 1
 
 mogrify -format jpg "$OUTPUT" || exit 2
-mogrify -resize 3840x "$OUTPUT" || exit 3
+mogrify -resize 3840x2160^ "$OUTPUT" || exit 3
 
 QUALITY="$(identify -verbose $OUTPUT | grep 'Image:\|Quality')"
 
