b2f3530bfca9b2f7e7de1eadef1cce296f60025c
Julius Enriquez <win8linux@gmail.com>
2025-05-19 00:08:35 +0800

    Use jxl format in mogrify, remove jhead dep

--- scripts/optimizeImage.sh
+++ scripts/optimizeImage.sh
@@ -5,7 +5,7 @@ OUTPUT="$2"
 
 cp "$INPUT" "$OUTPUT" || exit 1
 
-mogrify -format jpg "$OUTPUT" || exit 2
+mogrify -format jxl -strip "$OUTPUT" || exit 2
 mogrify -resize 3840x2160^ "$OUTPUT" || exit 3
 
 QUALITY=$(identify -format %Q  $OUTPUT) 
@@ -13,5 +13,3 @@ QUALITY=$(identify -format %Q  $OUTPUT)
 if [ $QUALITY -gt 90 ]; then
     mogrify -quality 90 "$OUTPUT" || exit 4
 fi
-
-jhead -autorot -de -di -du -c "$OUTPUT" || exit 5
